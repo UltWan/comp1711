bd87954518b39b7cd8f3a6b49571513096b234d6
Wah Wan [sc16wyrw] <sc16wyrw@comp-pc3008.leeds.ac.uk>
2016-09-28 15:46:14 +0100

    Submitted work

--- assignment_01/timetable.c
+++ assignment_01/timetable.c
@@ -1,3 +1,234 @@
 /*
 You should complete your assignment in here
 */
+
+// COMP 1711 Assignment 1
+// Ryan Wan, Wednesday 28th September 2016
+
+// Include the standard I/O library
+#include <stdio.h>
+
+// Task 1
+// Print out a list of modules and times
+int main()
+	{
+	printf("\n");
+	printf("Task 1:\n");
+	printf("Monday		COMP1711: Procedural Programming Lecture 1		10:00-11:00\n");
+	printf("Tuesday		COMP1421: Fundamental Mathematical Concepts Lecture 1	11:00-12:00\n");
+	printf("		COMP1711: Procedural Programming Lecture 2		13:00-14:00\n");
+	printf("Wednesday	COMP1421: Fundamental Mathematical Concepts Tutorial 1	09:00-10:00\n");
+	printf("		COMP1711 :Procedural Programming Lab Class 1		11:00-13:00\n");
+	printf("Thursday	COMP1911: Professional Computing Lecture 1		10:00-11:00\n");
+	printf("		COMP1421: Fundamental Mathematical Concepts Lecture 2	14:00-15:00\n");
+	printf("		COMP Tutorial						15:00-16:00\n");
+	printf("		COMP1211: Computer Architecture Lecture 1		16:00-17:00\n");
+	printf("Friday		COMP1911: Professional Computing Lecture 2		10:00-11:00\n");
+	printf("		COMP1211: Computer Architecture Lecture 2		11:00-12:00\n");
+	printf("		COMP Level 1 Employability				12:00-13:00\n");
+	printf("\n");
+
+
+// Task 2
+// Print out 6x8 table, which is 20 characters wide and 10 characters tall
+	printf("Task 2:\n");
+	printf("*--------------------*--------------------*--------------------*--------------------*--------------------*--------------------*\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("*--------------------*--------------------*--------------------*--------------------*--------------------*--------------------*\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("*--------------------*--------------------*--------------------*--------------------*--------------------*--------------------*\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("*--------------------*--------------------*--------------------*--------------------*--------------------*--------------------*\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("*--------------------*--------------------*--------------------*--------------------*--------------------*--------------------*\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("*--------------------*--------------------*--------------------*--------------------*--------------------*--------------------*\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("*--------------------*--------------------*--------------------*--------------------*--------------------*--------------------*\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("*--------------------*--------------------*--------------------*--------------------*--------------------*--------------------*\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("*--------------------*--------------------*--------------------*--------------------*--------------------*--------------------*\n");
+	printf("\n");
+
+
+// Task 3
+// Modified Task 1 and Task 2 codes, seen above, to add module codes, names and location into the boxes.
+// An additional row was added to fit in all the times.
+	printf("Task 3:\n");
+	printf("*--------------------*--------------------*--------------------*--------------------*--------------------*--------------------*\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     | 			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     | 	     Monday    	  |	 Tuesday       |     Wednesday	    |	   Thursday	 |	 Friday	      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("*--------------------*--------------------*--------------------*--------------------*--------------------*--------------------*\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |COMP1421:Fundamental|			 |		      |\n");
+	printf("|		     |			  |		       |Mathmatical	    |			 |		      |\n");
+	printf("|	09:00	     |			  |		       |Concepts Tutorial 1 |			 |		      |\n");
+	printf("|		     |			  |		       |    		    |			 |		      |\n");
+	printf("|		     |			  |		       |EC Stoner Active    |			 |		      |\n");
+	printf("|		     |			  |		       |Learning Lab(9.30a) |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("*--------------------*--------------------*--------------------*--------------------*--------------------*--------------------*\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |COMP1711:Procedural |		       |		    |COMP1911:		 |COMP1911:	      |\n");
+	printf("|		     |Programming	  |		       |		    |Professional 	 |Professional	      |\n");
+	printf("|	10:00	     |Lecture 1		  |		       |		    |Computing Lecture 2 |Computing Lecture 2 |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |Chemistry LT B(2.17)|		       |		    |Chemistry West Block|Roger Stevens LT 17 |\n");
+	printf("|		     |			  |		       |		    |LT F(G.74)		 |(7.17)	      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("*--------------------*--------------------*--------------------*--------------------*--------------------*--------------------*\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |COMP1421:Fundamental|COMP1711:Procedural |			 |COMP1211:Computer   |\n");
+	printf("|		     |			  |Mathematical        |Programming Lab     |			 |Architechure	      |\n");
+	printf("|	11:00	     |			  |Concepts Lecture 1  |Class 1		    |			 |Lecture 2  	      |\n");
+	printf("|		     |			  | 		       |		    |		   	 |		      |\n");
+	printf("|		     |			  |Roger Stevens LT 25 |EC Stoner DEC-10 Lab|			 |Roger Stevens LT 17 |\n");
+	printf("|		     |			  |(12.25)	       |(10.39)	A and B	    |			 |(7.17)	      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("*--------------------*--------------------*--------------------*--------------------*--------------------*--------------------*\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |COMP1711:Procedural |			 |COMP Level 1	      |\n");
+	printf("|		     |			  |		       |Programming Lab	    |			 |Employability	      |\n");
+	printf("|	12:00	     |			  |		       |Class 1		    |			 |	      	      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |EC Stoner DEC-10 Lab|			 |Roger Stevens LT 18 |\n");
+	printf("|		     |			  |		       |(10.39)	A and B	    |			 |(8.18)	      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("*--------------------*--------------------*--------------------*--------------------*--------------------*--------------------*\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |COMP1711:Procedural |		    |			 |		      |\n");
+	printf("|		     |			  |Programming	       |		    |			 |		      |\n");
+	printf("|	13:00	     |			  |Lecture 2 	       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |Chemistry West Block|		    |			 |		      |\n");
+	printf("|		     |			  |LT F(G.74)	       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("*--------------------*--------------------*--------------------*--------------------*--------------------*--------------------*\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |COMP1421:Fundamental|		      |\n");
+	printf("|		     |			  |		       |		    |Mathematical	 |		      |\n");
+	printf("|	14:00	     |			  |		       |		    |Concepts Lecture 2	 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |Chemistry LT B(2.17)|		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("*--------------------*--------------------*--------------------*--------------------*--------------------*--------------------*\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |COMP Tutorial	 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|	15:00	     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |EC Stoner(6.08)	 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("*--------------------*--------------------*--------------------*--------------------*--------------------*--------------------*\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |COMP1211:Computer	 |		      |\n");
+	printf("|		     |			  |		       |		    |Architechture	 |		      |\n");
+	printf("|	16:00	     |			  |		       |		    |Lecture 1	 	 |		      |\n");
+	printf("|		     |			  |		       |		    | 			 |		      |\n");
+	printf("|		     |			  |		       |		    |Roger Stevens LT 17 |		      |\n");
+	printf("|		     |			  |		       |		    |(7.17)		 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("|		     |			  |		       |		    |			 |		      |\n");
+	printf("*--------------------*--------------------*--------------------*--------------------*--------------------*--------------------*\n");
+	printf("\n");
+
+	return 0;
+	}
