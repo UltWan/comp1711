a2aef422420e61f655edf02df2b6831a1d95ab50
Ryan Wan <sc16wyrw@leeds.ac.uk>
2016-11-23 17:35:13 +0000

    Header

---
+++ assignment_05/decrypt.h
@@ -0,0 +1,12 @@
+// Header file for COMP1711 Assignment 5
+// Ryan Wan, Wednesday 22th November 2016
+
+
+// This unit has two routines, one for printing out encrypted data and the other for decrypting and printing out the decrypted data
+
+
+// Routine to print out in character format
+void printCharacter(int fileSize, char *charBuffer);
+
+// Routine to decrypt and print out the encrypted and decrypted text
+void printDecrypt(int fileSize, char *charBuffer);
